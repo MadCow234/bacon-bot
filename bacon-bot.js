b75712d847e0f5b95d41249ae737fb7aaa41e7d3
MadCow234 <MadCow234@gmail.com>
2018-04-15 23:31:52 -0500

    Testing winston logging on Heroku

--- bacon-bot.js
+++ bacon-bot.js
@@ -1,17 +1,15 @@
 // Create a Discord client
 const Discord = require("discord.js");
 const winston = require("winston");
-
-const log;
  
 if (process.env.NODE_ENV === 'production') {
-    log = winston.createLogger({
+    const log = winston.createLogger({
         transports: [
             new winston.transports.Console({ format: winston.format.simple() })
         ]
     })
 } else if (process.env.NODE_ENV === "development") {
-    log = winston.createLogger({
+    const log = winston.createLogger({
         transports: [
             new winston.transports.File({ filename: 'bacon-bot.log' })
         ]
