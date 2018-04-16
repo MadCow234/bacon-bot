c28999c7c57442984e4737af01196e75b58d908b
MadCow234 <MadCow234@gmail.com>
2018-04-16 01:40:34 -0500

    Reformat and change logging message

--- bacon-bot.js
+++ bacon-bot.js
@@ -1,19 +1,25 @@
 // Create a Discord client
-const Discord = require("discord.js");
-const Winston = require("winston");
+const Discord = require("discord.js").Client();
+const client = new Discord.Client();
 
 if (process.env.NODE_ENV !== 'production') {
-      require('dotenv').load();
+    require('dotenv').load();
 }
- 
+
+const Winston = require("winston");
 if (process.env.NODE_ENV === 'production') {
+    // Log to console in production because Heroku will add STDOUT to the log stream
     var log = new(Winston.Logger)({
         transports: [
             new Winston.transports.Console()
         ]
     });
+
+    // Configure CLI output
     log.cli();
+
 } else if (process.env.NODE_ENV === "development") {
+    // Log to a file in development
     var log = new(Winston.Logger)({
         transports: [
             new Winston.transports.File({ filename: 'bacon-bot.log' })
@@ -21,10 +27,9 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const client = new Discord.Client();
-
 const BOT_TOKEN = process.env.BOT_TOKEN;
-log.info(BOT_TOKEN)
 
-client.login(BOT_TOKEN).catch(error => console.log(error));
-// log.info("I'm logged in");
+client.login(BOT_TOKEN).catch(error => 
+    console.log(error)).then(
+        log.info("I'm logged in")
+    );
