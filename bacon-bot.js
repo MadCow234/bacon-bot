61a552113191cb55704783dd35aa0dc16d565a28
MadCow234 <MadCow234@gmail.com>
2018-04-15 20:27:48 -0500

    Add log statement to test Heroku logging

--- bacon-bot.js
+++ bacon-bot.js
@@ -1,7 +1,9 @@
 // Create a Discord client
 const Discord = require("discord.js");
+
 const client = new Discord.Client();
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 
 client.login(BOT_TOKEN);
+console.log("I'm logged in");
