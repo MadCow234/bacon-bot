d917f629ad1e3b8bb0082c75d02de8f74605ce9e
MadCow234 <MadCow234@gmail.com>
2018-04-20 15:19:16 -0500

    Enhance the getLogger() function

--- src/conf/logging.js
+++ src/conf/logging.js
@@ -1,23 +1,58 @@
 import { Logger, transports } from "winston";
+import * as fs from "fs";
 
 export function getLogger() {
+
     if (process.env.NODE_ENV === 'production') {
         // Log to console in production because Heroku will add STDOUT to the log stream
-        var logger = new Logger({
-            transports: [
-                new transports.Console()
-            ]
-        });
-        // Configure CLI output
-        logger.cli();
+        return getConsoleLogger();
+
+    } else if (process.env.NODE_ENV === "development") {
+
+        try {
+            // Attempt to create a /logs directory
+            fs.mkdirSync("./logs");
+
+        } catch (err) {
+            // If the /logs directory already exists, continue
+            // Otherwise, return a console logger
+            if (err.code !== "EEXIST") {
+                console.log(`An error has occurred that cannot be fixed (code: ${err.code})!`);
+                console.log(`Switching to console logging...`);
+                return getConsoleLogger();      
+            }
+        }
+
+        // As long as a /logs directory exists, return a file logger
+        return getFileLogger();
+        
+    } else {
+        console.log(`Cannot determine current Node environment! You may need to set a "process.env.NODE_ENV" variable in your .env file.`);
+        console.log(`Switching to console logging...`);
+        return getConsoleLogger();
     }
-    else if (process.env.NODE_ENV === "development") {
-        // Log to a file in development        
-        var logger = new Logger({
-            transports: [
-                new transports.File({ filename: 'bacon-bot.log' })
-            ]
-        });
-    }
+}
+
+function getConsoleLogger() {
+    // Create a console logger
+    var logger = new Logger({
+        transports: [
+            new transports.Console()
+        ]
+    });
+    // Configure CLI output
+    logger.cli();
+
+    return logger;
+}
+
+function getFileLogger() {    
+    // Create a file logger       
+    var logger = new Logger({
+        transports: [
+            new transports.File({ filename: './logs/bacon-bot.log' })
+        ]
+    });
+    
     return logger;
 }
