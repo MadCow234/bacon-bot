c6d8f57e62ed5c05cec6b4b86b359ae4bf98417d
MadCow234 <MadCow234@gmail.com>
2018-04-21 00:41:37 -0500

    Refactor

--- src/conf/logging.js
+++ src/conf/logging.js
@@ -3,47 +3,32 @@ import * as fs from "fs";
 
 export function getLogger() {
 
-    if (process.env.NODE_ENV === 'production') {
+    const nodeEnv = process.env.NODE_ENV;
+
+    if (nodeEnv === 'production') {
         // Log to console in production because Heroku will add STDOUT to the log stream
         return getConsoleLogger();
 
-    } else if (process.env.NODE_ENV === "development") {
+    } else if (nodeEnv === 'development' || nodeEnv === 'test') {
 
         try {
             // Attempt to create a /logs directory
-            fs.mkdirSync("./logs");
+            fs.mkdirSync('./logs');
 
         } catch (err) {
             // If the /logs directory already exists, continue
             // Otherwise, return a console logger
-            if (err.code !== "EEXIST") {
+            if (err.code !== 'EEXIST') {
                 console.log(`An error has occurred that cannot be fixed (code: ${err.code})!`);
                 console.log(`Switching to console logging...`);
                 return getConsoleLogger();      
             }
         }
 
-        // As long as a /logs directory exists, return a file logger
-        return getFileLogger('logs', 'bacon-bot');
-        
-    } else if (process.env.NODE_ENV === "test") {
-
-        try {
-            // Attempt to create a /logs directory
-            fs.mkdirSync("./logs");
-
-        } catch (err) {
-            // If the /logs directory already exists, continue
-            // Otherwise, return a console logger
-            if (err.code !== "EEXIST") {
-                console.log(`An error has occurred that cannot be fixed (code: ${err.code})!`);
-                console.log(`Switching to console logging...`);
-                return getConsoleLogger();      
-            }
-        }
+        var filename = nodeEnv === 'test' ? 'bacon-bot_test.log' : 'bacon-bot.log';
 
         // As long as a /logs directory exists, return a file logger
-        return getFileLogger('logs', 'bacon-bot_test');
+        return getFileLogger('logs', `${filename}`);
 
     } else {
         console.log(`Cannot determine current Node environment! You may need to set a "process.env.NODE_ENV" variable in your .env file.`);
@@ -69,7 +54,7 @@ function getFileLogger(directoryName, filename) {
     // Create a file logger       
     var logger = new Logger({
         transports: [
-            new transports.File({ filename: `./${directoryName}/${filename}.log` })
+            new transports.File({ filename: `./${directoryName}/${filename}` })
         ]
     });
     
